9e4a14338cde87d02e9d00ae373a3e94587710cc
kchranger <kchranger@gmail.com>
2025-03-13 15:07:37 +0900

    refactor: 기도 제목 응답 Json Key 변경

--- mocks/routes/PrayTitleRoutes.js
+++ mocks/routes/PrayTitleRoutes.js
@@ -5,13 +5,13 @@ const PrayerTitleRoutes = (server, db) => {
     const allPrayers = db.get("prayerTitles").value();
 
     if (!req.query["after"] || req.query["after"] === "0") {
-      res.json({ prayers: allPrayers.slice(0, 7) });
+      res.json({ prayerTitles: allPrayers.slice(0, 7) });
     } else if (req.query["after"] === "2025-02-12T08:47:29.563Z") {
-      res.json({ prayers: allPrayers.slice(7, 14) });
+      res.json({ prayerTitles: allPrayers.slice(7, 14) });
     } else if (req.query["after"] === "2024-12-28T07:39:45.612Z") {
-      res.json({ prayers: allPrayers.slice(14) });
+      res.json({ prayerTitles: allPrayers.slice(14) });
     } else {
-      res.json({ prayers: [] });
+      res.json({ prayerTitles: [] });
     }
   });
 };
