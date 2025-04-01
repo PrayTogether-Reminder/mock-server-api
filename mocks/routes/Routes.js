ef6bc431b7a905a118ed9e91e22c32791472d222
kchranger <kchranger@gmail.com>
2025-04-02 00:16:47 +0900

    refactor: 회원 API URL 변경

--- mocks/routes/Routes.js
+++ mocks/routes/Routes.js
@@ -2,14 +2,14 @@ const setupRoomsRoutes = require("./RoomsRoutes");
 const setupInvitationsRoutes = require("./InvitationsRoutes");
 const setupPrayersRoutes = require("./prayersRoutes");
 const setupNotificationsRoutes = require("./notificationsRoutes");
-const setupProfileRoutes = require("./profilesRoutes");
+const setupMemberRoutes = require("./membersRoutes");
 
 const setupRoutes = (server, db) => {
   setupRoomsRoutes(server, db);
   setupInvitationsRoutes(server, db);
   setupPrayersRoutes(server, db);
   setupNotificationsRoutes(server, db);
-  setupProfileRoutes(server, db);
+  setupMemberRoutes(server, db);
 };
 
 module.exports = setupRoutes;
--- mocks/routes/membersRoutes.js
+++ mocks/routes/membersRoutes.js
@@ -1,7 +1,7 @@
 const apiVersion = require("./apiVersion");
 
 const profilesRoutes = (server, db) => {
-  server.get(apiVersion + "/profiles/me", (req, res) => {
+  server.get(apiVersion + "/members/profiles", (req, res) => {
     const profile = db.get("profileData").value();
     console.log(JSON.stringify(profile, null, 2));
     return res.json(profile);
