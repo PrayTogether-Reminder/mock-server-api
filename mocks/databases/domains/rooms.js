214ca47a30ae0461612cb9ddf9d65ba6e45891eb
kchranger <kchranger@gmail.com>
2025-04-07 02:43:22 +0900

    rename: 방 목록 필드 이름 변경

--- mocks/databases/domains/rooms.js
+++ mocks/databases/domains/rooms.js
@@ -6,7 +6,7 @@ module.exports = () => {
         name: "2025 여키 기도방",
         memberCnt: 14,
         description: "2025년 여호수아 기획위원 중보기도방 입니다.",
-        createdTime: "2023-11-24T07:30:03Z",
+        joinedTime: "2023-11-24T07:30:03Z",
         isNotification: true,
       },
       {
@@ -14,7 +14,7 @@ module.exports = () => {
         name: "2025 셀그룹장 기도방",
         memberCnt: 9,
         description: "2025년 여호수아 셀그룹장 중보기도방 입니다.",
-        createdTime: "2025-05-05T08:10:38Z",
+        joinedTime: "2025-05-05T08:10:38Z",
         isNotification: false,
       },
       {
@@ -22,7 +22,7 @@ module.exports = () => {
         name: "청년부 리더십 기도방",
         memberCnt: 12,
         description: "청년부 리더십 팀을 위한 중보기도방입니다.",
-        createdTime: "2024-07-20T02:26:29Z",
+        joinedTime: "2024-07-20T02:26:29Z",
         isNotification: true,
       },
       {
@@ -30,7 +30,7 @@ module.exports = () => {
         name: "찬양팀 기도모임",
         memberCnt: 8,
         description: "주일 찬양팀을 위한 기도모임방입니다.",
-        createdTime: "2024-05-14T06:37:14Z",
+        joinedTime: "2024-05-14T06:37:14Z",
         isNotification: false,
       },
       {
@@ -38,7 +38,7 @@ module.exports = () => {
         name: "새가족 환영팀 기도방",
         memberCnt: 6,
         description: "새가족 환영팀 중보기도방입니다.",
-        createdTime: "2024-08-09T07:16:01Z",
+        joinedTime: "2024-08-09T07:16:01Z",
         isNotification: true,
       },
       {
@@ -46,7 +46,7 @@ module.exports = () => {
         name: "미디어팀 기도모임",
         memberCnt: 7,
         description: "미디어팀 사역을 위한 기도모임방입니다.",
-        createdTime: "2024-12-19T17:04:50Z",
+        joinedTime: "2024-12-19T17:04:50Z",
         isNotification: false,
       },
       {
@@ -54,7 +54,7 @@ module.exports = () => {
         name: "선교팀 중보기도방",
         memberCnt: 15,
         description: "해외선교팀을 위한 중보기도방입니다.",
-        createdTime: "2024-03-08T00:42:23Z",
+        joinedTime: "2024-03-08T00:42:23Z",
         isNotification: true,
       },
       {
@@ -62,7 +62,7 @@ module.exports = () => {
         name: "청년부 간사 기도모임",
         memberCnt: 5,
         description: "청년부 간사들의 기도모임방입니다.",
-        createdTime: "2024-09-06T06:35:01Z",
+        joinedTime: "2024-09-06T06:35:01Z",
         isNotification: false,
       },
       {
@@ -70,7 +70,7 @@ module.exports = () => {
         name: "교육부 교사 기도방",
         memberCnt: 18,
         description: "교육부 교사들을 위한 중보기도방입니다.",
-        createdTime: "2025-01-23T22:32:01Z",
+        joinedTime: "2025-01-23T22:32:01Z",
         isNotification: true,
       },
       {
@@ -78,7 +78,7 @@ module.exports = () => {
         name: "성경공부 리더 기도모임",
         memberCnt: 11,
         description: "성경공부 리더들의 기도모임방입니다.",
-        createdTime: "2024-04-05T07:34:38Z",
+        joinedTime: "2024-04-05T07:34:38Z",
         isNotification: false,
       },
       {
@@ -86,7 +86,7 @@ module.exports = () => {
         name: "예배팀 중보기도방",
         memberCnt: 13,
         description: "예배팀을 위한 중보기도방입니다.",
-        createdTime: "2024-10-25T05:22:23Z",
+        joinedTime: "2024-10-25T05:22:23Z",
         isNotification: true,
       },
       {
@@ -94,7 +94,7 @@ module.exports = () => {
         name: "청년부 셀리더 기도방",
         memberCnt: 20,
         description: "청년부 셀리더들의 기도모임방입니다.",
-        createdTime: "2024-06-19T22:30:32Z",
+        joinedTime: "2024-06-19T22:30:32Z",
         isNotification: false,
       },
       {
@@ -102,7 +102,7 @@ module.exports = () => {
         name: "봉사팀 기도모임",
         memberCnt: 16,
         description: "교회 봉사팀을 위한 기도모임방입니다.",
-        createdTime: "2024-11-06T04:26:29Z",
+        joinedTime: "2024-11-06T04:26:29Z",
         isNotification: true,
       },
       {
@@ -110,7 +110,7 @@ module.exports = () => {
         name: "중보기도팀 모임방",
         memberCnt: 25,
         description: "교회 중보기도팀 모임방입니다.",
-        createdTime: "2024-08-25T08:44:50Z",
+        joinedTime: "2024-08-25T08:44:50Z",
         isNotification: false,
       },
       {
@@ -118,7 +118,7 @@ module.exports = () => {
         name: "청년부 찬양팀 기도방",
         memberCnt: 10,
         description: "청년부 찬양팀을 위한 기도방입니다.",
-        createdTime: "2024-05-27T05:56:52Z",
+        joinedTime: "2024-05-27T05:56:52Z",
         isNotification: true,
       },
     ],
