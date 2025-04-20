efd627bd4ae2ff10f004daae1d58379a271ec78c
kchranger <kchranger@gmail.com>
2025-04-21 00:08:57 +0900

    fix: 사용하지 않는 Notifications Route 삭제

--- mocks/routes/Routes.js
+++ mocks/routes/Routes.js
@@ -1,14 +1,12 @@
 const setupRoomsRoutes = require("./RoomsRoutes");
 const setupInvitationsRoutes = require("./InvitationsRoutes");
 const setupPrayersRoutes = require("./prayersRoutes");
-const setupNotificationsRoutes = require("./notificationsRoutes");
 const setupMemberRoutes = require("./membersRoutes");
 
 const setupRoutes = (server, db) => {
   setupRoomsRoutes(server, db);
   setupInvitationsRoutes(server, db);
   setupPrayersRoutes(server, db);
-  setupNotificationsRoutes(server, db);
   setupMemberRoutes(server, db);
 };
 
